d5a262d86815699e1e6813513182818ff3b08eb6
Adrian <radu.p1995@yahoo.com>
2025-06-07 16:29:45 +0300

    asdad

--- app/layout.tsx
+++ app/layout.tsx
@@ -137,6 +137,26 @@ export default function RootLayout({
             `,
           }}
         />
+
+        {/* Meta Pixel Code */}
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1357415508822546');
+            fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {/* Google Tag Manager (noscript) */}
@@ -149,6 +169,19 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Meta Pixel Code (noscript) */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=1357415508822546&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code (noscript) */}
+        
         {children}
         <Toaster />
       </body>
