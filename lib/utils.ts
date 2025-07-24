929a5ba9c022ad2459c0f86747618bf49ed9f5d7
Code-W-A <radu.p1995@yahoo.com>
2025-07-24 10:48:42 +0300

    adaugat badge pe mobile si  nr inmatriculare este mereu de forma DD88SSS

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,6 +13,6 @@ export function cn(...inputs: ClassValue[]) {
  */
 export function normalizeLicensePlate(input: string): string {
   return input
-    .replace(/[\s\.\-]/g, '') // Elimină spații, puncte și cratime
+    .replace(/[^A-Za-z0-9]/g, '') // Păstrează DOAR litere și cifre, elimină ORICE altceva
     .toUpperCase() // Convertește la majuscule
 }
